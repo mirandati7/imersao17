bbc01cf3bc0a1ee36664e86d87152c4d1b2356e0
alex-mr7 <alex@mr7ti.com.br>
2024-01-30 09:56:01 -0300

    Projeto GOLANG

---
+++ goapi/internal/database/category_db.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/mirandati7/imersao17/goapi/internal/entity"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{db: db}
+}
+
+func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := cd.db.Query("select id, name from categories")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categories []*entity.Category
+	for rows.Next() {
+		var category entity.Category
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+
+		categories = append(categories, &category)
+
+	}
+	return categories, nil
+
+}
+
+func (cd *CategoryDB) GetCategory(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := cd.db.QueryRow("select id, name from categories where id = ?", id).Scan(&category.ID, &category.Name)
+
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+
+}
+
+func (cd *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := cd.db.Exec("INSERT INTO categories (id, name) VALUES (?, ?)", category.ID, category.Name)
+
+	if err := err; err != nil {
+		return "", err
+	}
+	return category.ID, nil
+
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+# Install Go 
+https://royzsec.medium.com/install-go-1-21-0-in-ubuntu-22-04-2-in-5-minutes-468a5330c64e
